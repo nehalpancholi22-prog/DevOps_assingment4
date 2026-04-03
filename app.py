09d8bdf7bfea1651058ea2314af3a62112cdd709
pancholinehal <nehalpancholi2023msc@davvscsit.in>
2026-04-03 21:35:39 +0530

    Added Flask app with pink UI

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/api')
+def api():
+    return jsonify([
+        {"name": "item1"},
+        {"name": "item2"}
+    ])
+
+if __name__ == '__main__':
+    app.run(debug=True)
