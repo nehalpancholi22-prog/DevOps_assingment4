f8b44ab9c7f7ba6d5651d8186c0dcf636b659df0
pancholinehal <nehalpancholi2023msc@davvscsit.in>
2026-04-03 22:51:39 +0530

    Reset to previous state

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,13 @@ def api():
         {"name": "item1"},
         {"name": "item2"}
     ])
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    itemName = request.form.get("itemName")
+    itemDescription = request.form.get("itemDescription")
+
+    return f"Received {itemName} - {itemDescription}"
 
 if __name__ == '__main__':
     app.run(debug=True)
